d8c352bf5a231aad33f5bfdfe0b0d91aeaf59918
Peter <peter.dyumin@gmail.com>
2023-07-02 01:14:09 +0300

    Work on improved version: fix clustering

--- improved.js
+++ improved.js
@@ -1,21 +1,24 @@
-const numbers = [1, 2, 10, 4, -10, -9, 8, 16, 22, 43, 49, 0, 34, 32, 52, 68, 92].sort(
+const numbers = [1, 2, 10, 4, -10, 8, 16, 22, 43, 49, 0, 95, 34, 32, 52, 68, 92].sort(
   (a, b) => a - b,
 );
 
-function fnn(array = [], r = 10, val = array[0] || null, cluster = [], clusters = []) {
-  if (array.length === 0 || val === null) {
-    return clusters;
+function fnn(array = [], r = 10, prev = null, cluster = [], clusters = [], i = 0) {
+  if (array.length === 0) {
+    return [...clusters, cluster];
   }
   const [p, ...rest] = array;
-  console.log(val, p);
-  if ((p - val) < r) {
-    cluster.push(p);
-    console.log('add', p);
-  } else {
-    console.log('next', rest[rest.indexOf(p) + 1]);
-    return fnn(rest, r, rest[rest.indexOf(p) + 1] || null, [p], [...clusters, cluster]);
+  console.log(`new iteration (${i}), current value: ${p}, previous value: ${prev}`);
+  if (prev === null) {
+    console.log(` > add current value ${p} to cluster (previous is null / iteration ${i})`);
+    return fnn(rest, r, p, [p], clusters, i + 1);
   }
-  return fnn(rest, r, p, cluster, clusters);
+  if ((p - prev) < r) {
+    console.log(` > add current value ${p} to cluster (condition is true)`);
+    cluster.push(p);
+    return fnn(rest, r, p, cluster, clusters, i + 1);
+  }
+  console.log(` > start a new cluster with value ${p}, store cluster [${cluster}]`);
+  return fnn(rest, r, p, [p], [...clusters, cluster], i + 1);
 }
 
 const result = fnn(numbers, 5);
