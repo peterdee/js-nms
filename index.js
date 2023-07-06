ba287ac7d4568f7561b8a526c5cc8c08e9acac6b
Peter <p.dyumin@dunice.net>
2023-07-06 12:55:52 +0300

    Adjustments for the edge cases

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const pointsArray = [
   new Point(302, 15, 27),
   new Point(305, 17, 64),
   new Point(418, 236, 43),
-  new Point(306, 29, 92), 
+  new Point(306, 29, 92),
   new Point(307, 20, 96),
   new Point(308, 23, 42),
   new Point(308, 24, 20),
@@ -17,21 +17,24 @@ const pointsArray = [
   new Point(280, 450, 52),
 ];
 
-const edgeCase = [
-  new Point(111, 122, 45), 
-  new Point(100, 100, 22), 
-  new Point(101, 103, 90),
-  new Point(103, 97, 93),
-  new Point(105, 100, 50),
-  new Point(105, 107, 55),
-];
+// const edgeCase = [
+//   new Point(111, 122, 45),
+//   new Point(100, 100, 22),
+//   new Point(101, 103, 90),
+//   new Point(103, 97, 93),
+//   new Point(105, 100, 50),
+//   new Point(105, 107, 55),
+// ];
 
+/**
+ * Combine clusters if necessary
+ * @param {Point[]} cluster - current cluster
+ * @param {Point[][]} clusters - array of clusters
+ * @param {number} radius - point radius
+ * @returns {Point[][]}
+ */
 function combineClusters(cluster = [], clusters = [], radius = 10) {
   const [lastCluster = []] = clusters.slice(-1);
-  const [firstPoint = null] = cluster;
-  if (!firstPoint || lastCluster.length === 0) {
-    return [...clusters, cluster];
-  }
   let combine = false;
   for (let i = 0; i < lastCluster.length; i += 1) {
     const lastClusterPoint = lastCluster[i];
@@ -47,21 +50,12 @@ function combineClusters(cluster = [], clusters = [], radius = 10) {
       break;
     }
   }
-  // for (let i = 0; i < lastCluster.length; i += 1) {
-  //   const point = lastCluster[i];
-  //   if ((firstPoint.x - point.x) < radius
-  //     && Math.abs(firstPoint.y - point.y) < radius) {
-  //     combine = true;
-  //     break;
-  //   }
-  // }
-  if (!combine) {
-    return [...clusters, cluster];
-  }
-  return [
-    ...clusters.slice(0, clusters.length - 2),
-    [...lastCluster, ...cluster].sort((a, b) => a.x - b.x || a.y - b.y),
-  ];
+  return !combine
+    ? [...clusters, cluster]
+    : [
+      ...clusters.slice(0, clusters.length - 2),
+      [...lastCluster, ...cluster].sort((a, b) => a.x - b.x || a.y - b.y),
+    ];
 }
 
 /**
@@ -70,9 +64,8 @@ function combineClusters(cluster = [], clusters = [], radius = 10) {
  * @param {number} radius - point radius
  * @param {Point | null} previous - previous value for comparison
  * @param {Point[]} cluster - current point cluster
- * @param {Point[]} selected - selected points from each cluster (based on intensity)
+ * @param {Point[][]} clusters - array of clusters
  * @param {boolean} isSorted - indicates if passed array is sorted
- * @param {Point[][]} clusters
  * @returns {Point[]}
  */
 function nms(
@@ -80,9 +73,8 @@ function nms(
   radius = 10,
   previous = null,
   cluster = [],
-  selected = [],
-  isSorted = false,
   clusters = [],
+  isSorted = false,
 ) {
   if (!isSorted) {
     return nms(
@@ -90,57 +82,46 @@ function nms(
       radius,
       previous,
       cluster,
-      selected,
-      true,
       clusters,
+      true,
     );
   }
   if (array.length === 0) {
     console.log(combineClusters(cluster, clusters, radius));
-    return [...selected, cluster.sort((a, b) => b.intensity - a.intensity)[0]];
+    return combineClusters(cluster, clusters, radius).map(
+      (element) => element.sort((a, b) => b.intensity - a.intensity)[0],
+    );
   }
   const [current, ...rest] = array;
   if (previous === null) {
-    return nms(rest, radius, current, [current], selected, true, clusters);
+    return nms(rest, radius, current, [current], clusters, true);
   }
   if ((current.x - previous.x) < radius
     && Math.abs(current.y - previous.y) < radius) {
     cluster.push(current);
-    return nms(rest, radius, current, cluster, selected, true, clusters);
+    return nms(rest, radius, current, cluster, clusters, true);
   }
-  
   if (clusters.length > 0) {
     return nms(
       rest,
       radius,
       current,
       [current],
-      [
-        ...selected,
-        cluster.sort((a, b) => b.intensity - a.intensity)[0],
-      ],
-      true,
       combineClusters(cluster, clusters, radius),
+      true,
     );
   }
-
+  clusters.push(cluster);
   return nms(
     rest,
     radius,
     current,
     [current],
-    [
-      ...selected,
-      cluster.sort((a, b) => b.intensity - a.intensity)[0],
-    ],
+    clusters,
     true,
-    [
-      ...clusters,
-      cluster,
-    ],
   );
 }
 
 console.time('nms');
-console.log(nms(pointsArray, 5));
+console.log(nms(pointsArray, 15));
 console.timeEnd('nms');
